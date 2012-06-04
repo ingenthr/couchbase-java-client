b0ed0a8a1034c5b2870571870bfe1bccb8692172
Matt Ingenthron <ingenthr@cep.net>
2012-06-03 22:32:16 -0700

    WIP: more for optimized set test
    Change-Id: I72426a16cc3fef0ddcb227dfb7ead27cfc6bb051

--- src/test/java/net/spy/memcached/protocol/binary/OptimizedStoreVbucketTest.java
+++ src/test/java/net/spy/memcached/protocol/binary/OptimizedStoreVbucketTest.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (C) 2006-2009 Dustin Sallings
- * Copyright (C) 2009-2011 Couchbase, Inc.
+ * Copyright (C) 2009-2012 Couchbase, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,14 +23,24 @@
 
 package net.spy.memcached.protocol.binary;
 
-import com.couchbase.client.CouchbaseClient;
 import com.couchbase.client.CouchbaseConnectionFactory;
+import com.couchbase.client.FailInjectingCouchbaseConnectionFactory;
+import com.couchbase.client.TestingCouchbaseClient;
+import java.lang.Boolean;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Arrays;
-import net.spy.memcached.BinaryClientTest;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import net.spy.memcached.BuildInfo;
+import net.spy.memcached.ClientBaseCase;
 import net.spy.memcached.ConnectionFactory;
 import net.spy.memcached.TestConfig;
+import net.spy.memcached.internal.OperationFuture;
+import net.spy.memcached.ops.OperationState;
+import net.spy.memcached.ops.OperationStatus;
 import net.spy.memcached.ops.StoreType;
+import org.junit.AfterClass;
 
 /**
  * A test for optimized sets.
@@ -38,30 +48,81 @@ import net.spy.memcached.ops.StoreType;
  * This should generate an optimized set of sets, with one set destined for the
  * wrong vbucket in the middle of the group.  This wrong 
  */
-public class OptimizedStoreVbucketTest extends BinaryClientTest {
+public class OptimizedStoreVbucketTest extends ClientBaseCase {
+  private TestingCouchbaseClient tclient;
   @Override
   protected void initClient() throws Exception {
-    initClient(new CouchbaseConnectionFactory(Arrays.asList(URI.create("http://"
+    initClient(new FailInjectingCouchbaseConnectionFactory(Arrays.asList(URI.create("http://"
         + TestConfig.IPV4_ADDR + ":8091/pools")), "default", ""));
   }
 
   @Override
-  protected String getExpectedVersionSource() {
-    if (TestConfig.IPV4_ADDR.equals("127.0.0.1")) {
-      return "/127.0.0.1:11210";
-    }
-    return TestConfig.IPV4_ADDR + ":11210";
-  }
-
-  @Override
   protected void initClient(ConnectionFactory cf) throws Exception {
-    client = new CouchbaseClient((CouchbaseConnectionFactory) cf);
+    tclient = new TestingCouchbaseClient((CouchbaseConnectionFactory) cf);
+    client = tclient;
   }
 
-  public void testOptimizedStore() {
+  public void failedtotestOptimizedStore() {
     OptimizedSetImpl toTry = new OptimizedSetImpl(new StoreOperationImpl(
       StoreType.set, "key", 0, 10,
       "value".getBytes(), 0, null)); // flags, expiration, data, cas
+
+    toTry.addOperation(new StoreOperationImpl(
+      StoreType.set, "key2", 0, 10,
+      "value".getBytes(), 0, null));
+
+    toTry.initialize();
+    assert toTry.getState() == OperationState.WRITE_QUEUED : "Write was not queued as expected.";
+
+    tclient.enqueueTestOperation("key", toTry);
+  }
+
+  public void testOptimizedStore() throws InterruptedException, ExecutionException {
+
+    System.err.println("Using spymemcached build " + BuildInfo.GIT_HASH);
+
+    ArrayList<OperationFuture<Boolean>> completed = new ArrayList<OperationFuture<Boolean>>();
+
+    OperationFuture<Boolean> lastBogus, lastReal;
+
+    System.err.println("Setting regular and bogus keys.  Bogus should retry.");
+    for (int i=1; i<19; i++) {
+      String kv = "iter" + i;
+      client.set(kv, 0, kv);
+      if (i%10 == 0) {
+        OperationFuture<Boolean> justSet = client.set("bogus" + i, 0, kv);
+        completed.add(justSet);
+      }
+    }
+
+    // verify they're all successful
+    System.err.println("Verifying all have been set successfully.");
+    for (OperationFuture doneOpf : completed) {
+      int times = 0;
+      OperationStatus status;
+//      long startTime = System.currentTimeMillis();
+//      doneOpf.get();
+//      System.err.println("Time to get() from the future:" + (startTime - System.currentTimeMillis()));
+      status = doneOpf.getStatus();
+      do {
+
+        if (times == 30) {
+          fail("Tried " + times + " times and did not see a successful set with key: \"" + doneOpf.getKey() + "\".  Operation status message: " + status.getMessage());
+        }
+        if (times > 0) {
+          double backoffMillis = Math.pow(2, times);
+          backoffMillis = Math.min(1000, backoffMillis); // 1 sec max
+          Thread.sleep((int) backoffMillis);
+          System.err.println("Backing off to get a successful set, tries so far: " + times);
+        }
+        times++;
+      } while (!status.isSuccess());
+
+      Object result = client.get(doneOpf.getKey());
+      assertNotNull("Key was not set " + doneOpf.getKey(), result);
+
+    }
+
   }
 
 }
